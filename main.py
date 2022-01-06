3255d301e52b8432dfd496b67ddec07eeecf8a5d
David Aminifard <daminifard@live.com>
2022-01-06 15:23:04 -0800

    added google sheets functionality

--- main.py
+++ main.py
@@ -2,7 +2,23 @@ import praw
 import os
 import pandas as pd
 import json
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
 
+SCOPE = [
+    # Used to send and retrieve data to and from Google Sheets
+    "https://spreadsheets.google.com/feeds",
+    'https://www.googleapis.com/auth/spreadsheets',
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive",
+]
+
+creds = ServiceAccountCredentials.from_json_keyfile_name(os.path.join("config", "google_sheets_creds.json"), SCOPE)
+
+client = gspread.authorize(creds)
+
+sheet = client.open("reddit_data").sheet1
+         
 conf_path = os.path.join('config', 'conf.json')
 with open(conf_path, "r") as conf_file:
     config_data = json.loads(conf_file.read())
@@ -16,4 +32,7 @@ reddit = praw.Reddit(
 
 
 if __name__=="__main__":
-    print(reddit)
+    for submission in reddit.subreddit("UCSD").hot(limit=10):
+        print(submission.title)
+        
+    print(pd.DataFrame(sheet.get_all_records()))
