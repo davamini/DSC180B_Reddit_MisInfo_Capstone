31d7f73df2d1d2623133388a6874c864ab84eea6
David Aminifard <daminifard@live.com>
2022-01-07 16:04:54 -0800

    updates based of input from sheets now

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from re import sub
 import praw
 import os
 import pandas as pd
@@ -5,6 +6,10 @@ import json
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 import datetime
+import time
+import sys
+
+# popular, rising popularity (year), covid related, politics, news
 
 SCOPE = [
     # Used to send and retrieve data to and from Google Sheets
@@ -18,8 +23,15 @@ creds = ServiceAccountCredentials.from_json_keyfile_name(os.path.join("config", 
 
 client = gspread.authorize(creds)
 
-worksheet = client.open("reddit_data").sheet1
-         
+
+subreddits_worksheet = client.open("database").worksheet('subreddits')
+
+submission_data_worksheet = client.open("database").worksheet('submission_data')
+
+subreddits_df = pd.DataFrame(subreddits_worksheet.get_all_records())
+
+
+
 conf_path = os.path.join('config', 'conf.json')
 with open(conf_path, "r") as conf_file:
     config_data = json.loads(conf_file.read())
@@ -32,15 +44,32 @@ reddit = praw.Reddit(
 )
 
 
+SUBMISSION_LIMIT = 100
+SLEEP_TIME = .3
+
 if __name__=="__main__":
     reddit_data = []
-    subreddit = "UCSD"
-    for submission in reddit.subreddit(subreddit).hot(limit=10):
-        reddit_data.append((subreddit, submission.title, submission.selftext, submission.url, str(datetime.datetime.fromtimestamp(submission.created))))
-        
-    reddit_data_df = pd.DataFrame(reddit_data, columns=["Subreddit", "Title", "Text", "URL", "Date Created"])
+    topics = subreddits_df.columns
+    for topic in topics:
+        current_subreddits = subreddits_df[topic].str.replace('r/', '')
+        for subreddit in current_subreddits:
+            for index, submission in enumerate(reddit.subreddit(subreddit).hot(limit=SUBMISSION_LIMIT)):
+                print_str = f"Getting data for subreddit: r/{subreddit}; Progress: {round((index/SUBMISSION_LIMIT)*100, 3)}%"
+                print(print_str, end="\r")
+                reddit_data.append((subreddit, 
+                                    submission.title,
+                                    submission.author.name, 
+                                    submission.selftext, 
+                                    submission.url, 
+                                    str(datetime.datetime.fromtimestamp(submission.created)),
+                                    submission.downs,
+                                    submission.ups,
+                                    submission.upvote_ratio
+                                    ))
+                
+                time.sleep(SLEEP_TIME)
+                
+    reddit_data_df = pd.DataFrame(reddit_data, columns=["Subreddit", "Title", "Author", "Text", "URL", "Date Created", "Upvotes", "Downvotes", "Upvote Ratio"])
     
     
-    worksheet.update([reddit_data_df.columns.values.tolist()] + reddit_data_df.values.tolist())
-        
-    print(pd.DataFrame(worksheet.get_all_records()))
+    submission_data_worksheet.update([reddit_data_df.columns.values.tolist()] + reddit_data_df.values.tolist())
