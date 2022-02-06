0a166441f89511c8e0027a99e5dfac7986f6194a
David Aminifard <daminifard@live.com>
2022-02-06 14:45:58 -0800

    added test functionality

--- run.py
+++ run.py
@@ -19,51 +19,78 @@ SCOPE = [
     "https://www.googleapis.com/auth/drive",
 ]
 
-creds = ServiceAccountCredentials.from_json_keyfile_name(os.path.join("config", "google_sheets_creds.json"), SCOPE)
-
-client = gspread.authorize(creds)
-
-
-subreddits_worksheet = client.open("database").worksheet('subreddits')
-
-submission_data_worksheet = client.open("database").worksheet('submission_data')
-
-subreddits_df = pd.DataFrame(subreddits_worksheet.get_all_records()) # Subreddits to analyze
-
-subreddits_data_df = pd.DataFrame(submission_data_worksheet.get_all_records()) # Actual subreddit data
-
-append_location = subreddits_data_df.shape[0] + 2
-top_posts_range = "year"
-submissions_already_aquired = set()
-
 try:
-    submissions_already_aquired = set(subreddits_data_df["ID"].tolist())
-    if len(pd.to_datetime(subreddits_data_df["Date Created"]).dt.year.unique()) > 1:
-        top_posts_range = "month"
-except KeyError as e:
-    print(f"No data detected in {submission_data_worksheet.title} worksheet")
-    
-conf_path = os.path.join('config', 'conf.json')
-with open(conf_path, "r") as conf_file:
-    config_data = json.loads(conf_file.read())
+    creds = ServiceAccountCredentials.from_json_keyfile_name(os.path.join("config", "google_sheets_creds.json"), SCOPE)
 
-reddit = praw.Reddit(
-    # Creating Reddit object
-    client_id=config_data["client_id"],
-    client_secret=config_data["client_secret"],
-    user_agent="DSC180B capstone project",
-)
+    client = gspread.authorize(creds)
 
-iffy_data = pd.read_csv("iffy+ 2021-03 - EmbedIffy+.tsv", sep="\t")
 
-mis_info_domains = set(iffy_data["Domain"])
+    subreddits_worksheet = client.open("database").worksheet('subreddits')
+
+    submission_data_worksheet = client.open("database").worksheet('submission_data')
+
+    subreddits_df = pd.DataFrame(subreddits_worksheet.get_all_records()) # Subreddits to analyze
+
+    subreddits_data_df = pd.DataFrame(submission_data_worksheet.get_all_records()) # Actual subreddit data
+
+    append_location = subreddits_data_df.shape[0] + 2
+    top_posts_range = "year"
+    submissions_already_aquired = set()
+
+    try:
+        submissions_already_aquired = set(subreddits_data_df["ID"].tolist())
+        if len(pd.to_datetime(subreddits_data_df["Date Created"]).dt.year.unique()) > 1:
+            top_posts_range = "month"
+    except KeyError as e:
+        print(f"No data detected in {submission_data_worksheet.title} worksheet")
+        
+    conf_path = os.path.join('config', 'conf.json')
+    with open(conf_path, "r") as conf_file:
+        config_data = json.loads(conf_file.read())
+
+    reddit = praw.Reddit(
+        # Creating Reddit object
+        client_id=config_data["client_id"],
+        client_secret=config_data["client_secret"],
+        user_agent="DSC180B capstone project",
+    )
+
+    iffy_data = pd.read_csv(os.path.join("data", "iffy+ 2021-03 - EmbedIffy+.tsv"), sep="\t")
+
+    mis_info_domains = set(iffy_data["Domain"])
+
+except Exception as e:
+    print(f"Error: {e}\nContinuing...")
 
 
 SUBMISSION_LIMIT = 1000
 SLEEP_TIME = 0.005
 
-
+# python run.py test
 if __name__=="__main__":
+    
+    if sys.argv[1] == "test":
+        """
+        Produces a small output based on test data
+        """
+        gsheet_submission_data = pd.read_csv(os.path.join("data", "test_sample.csv"))
+        
+        mis_info_posts = gsheet_submission_data.loc[gsheet_submission_data["Is Misinformation"] == "Detected"].reset_index(drop=True)
+
+        curr_fig = mis_info_posts["Subreddit"].value_counts(
+        ).plot(kind="barh", 
+            ylabel="# of Misinformation Links Detected", 
+            xlabel="Subreddit", 
+            title="# of Misinformation URLs found per Subreddit").get_figure()
+
+        curr_fig.savefig(os.path.join("outputs", "# of Misinformation URLs found per Subreddit"), bbox_inches='tight')
+   
+        
+        sys.exit(0)
+        
+    # Gets subreddit submission data based on parameters in a google sheet
+    # Detects misinformation based on iffy.news
+    # Uploads data to a google sheet database
     reddit_data = []
     topics = subreddits_df.columns
     for topic in topics:
